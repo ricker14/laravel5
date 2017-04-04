baea081335a7f05ce22630eaf59981a9cbccd1aa
Rick O'Connor <roconnor@nasba.org>
2017-04-04 13:05:21 -0500

    Added a "broken" link to test later.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -84,11 +84,7 @@
 
                 <div class="links">
                     <a href="newRoute">New Route</a>
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="brokenRoute">Broken Route</a>
                 </div>
             </div>
         </div>
